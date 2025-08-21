a17128b4372639b027d14fa7df2e84637ed76e85
Nestor Toscano <toscanonestor07@gmail.com>
2025-08-21 15:33:54 -0500

    changed nav bar on mobile to slide animation

--- nestortoscano.com/script.js
+++ nestortoscano.com/script.js
@@ -1,6 +1,11 @@
-const toggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('nav');
+// const toggle = document.querySelector('.menu-toggle');
+// const nav = document.querySelector('nav');
 
-toggle.addEventListener('click', function() {
-  nav.classList.toggle('active');
-});
+// toggle.addEventListener('click', function() {
+//   nav.classList.toggle('active');
+// });
+
+$(".menu-toggle").click(function() {
+  // $("nav").toggleClass("active");
+  $(".mobile-menu ul").slideToggle(200);
+})
